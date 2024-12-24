38c7a1f820dee73368d0bc04f89eb29f7a419d67
chi tseng <vitus.tseng@gmail.com>
2024-12-24 16:12:56 +0800

    split from DiaryTree

---
+++ DiaryTreeNode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace personal_note
+{
+    internal class DiaryTreeNode
+    {
+        public int year, month, day;
+        public List<DiaryNode> nodes;
+        public DiaryTreeNode sibling;
+        public DiaryTreeNode child;
+
+        public DiaryTreeNode(int year, int month, int day)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.nodes = new List<DiaryNode>();
+            this.sibling = null;
+            this.child = null;
+        }
+    }
+}
