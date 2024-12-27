5cf198d6baba327f7887b9e5344b3bc53c8f58ab
ChengHao1611 <df100109@mymail.com>
2024-12-27 15:33:58 +0800

    在for迴圈加入int

--- Form1.cs
+++ Form1.cs
@@ -413,8 +413,7 @@ namespace personal_note
         private void resetLabel()
         {
             // reset the visibility of the labels
-            int date = 1;
-            for (date = 1; date <= currentMonthDays; date++)
+            for (int date = 1; date <= currentMonthDays; date++)
             {
                 DiaryTreeNode dayNode = DiaryTree.searchDay(date, monthNode);
                 foreach (DiaryNode diaryNode in dayNode.nodes)
